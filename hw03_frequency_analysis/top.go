7d7c8883081fe42088344f53395b092b5b83f117
Evgeniy Korolev <korolev_es@nlmk.com>
2025-06-27 01:09:12 +0300

    HW3 Complete

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,48 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var re = regexp.MustCompile(`((\s+-\s+)|[\"?!;:.,\s])`)
+
+type Word struct {
+	text  string
+	count int
+}
+
+func Top10(str string) []string {
+	lowerStr := strings.ToLower(str)
+	words := re.Split(lowerStr, -1)
+	counter := make(map[string]int)
+	for _, s := range words {
+		if v, ok := counter[s]; ok {
+			counter[s] = v + 1
+		} else {
+			counter[s] = 1
+		}
+	}
+
+	sortSlice := make([]Word, 0, len(counter))
+	for k, v := range counter {
+		sortSlice = append(sortSlice, Word{k, v})
+	}
+
+	sort.Slice(sortSlice, func(i, j int) bool {
+		if sortSlice[i].count != sortSlice[j].count {
+			return sortSlice[i].count > sortSlice[j].count
+		}
+		return sortSlice[i].text < sortSlice[j].text
+	})
+
+	var res []string
+	for i := 0; i < 11 && i < len(sortSlice); i++ {
+		if sortSlice[i].text != "" {
+			res = append(res, sortSlice[i].text)
+		}
+	}
+
+	return res
 }
