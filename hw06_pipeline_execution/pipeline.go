ff82787873b61aba146cbdaef6381fec454b8f74
Евгений Королев <korolev.work.email@gmail.com>
2025-09-08 17:17:20 +0700

    hw06 done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,43 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	controlledIn := make(chan interface{})
+
+	go func() {
+		defer close(controlledIn)
+		for data := range in {
+			select {
+			case <-done:
+				return
+			case controlledIn <- data:
+			}
+		}
+	}()
+
+	out := In(controlledIn)
+	for _, stage := range stages {
+		out = stage(out)
+	}
+
+	var result []interface{}
+	for {
+		select {
+		case val, ok := <-out:
+			if !ok {
+				o := make(chan interface{})
+				go func() {
+					for _, v := range result {
+						o <- v
+					}
+					close(o)
+				}()
+				return o
+			}
+			result = append(result, val)
+		case <-done:
+			empty := make(chan interface{})
+			close(empty)
+			return empty
+		}
+	}
 }
